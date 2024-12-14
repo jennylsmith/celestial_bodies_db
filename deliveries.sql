49be26396fd0e084dca9530170b00b1e300350c3
Jenny Leopoldina Smith <jennyl.smith12@gmail.com>
2024-12-14 01:22:29 +0000

    added sql scripts to insert rows into three different tables

---
+++ deliveries.sql
@@ -0,0 +1,4 @@
+INSERT INTO deliveries(galaxy_id, name, description, address, planet_id)
+VALUES(1,'farnsworth', 'fake poop', '250 kyz dr, palm', 6),
+(3, 'mother','anchovies', '445 lop ave, oak', 1),
+(5, 'calculon','crown', '121 red st, redwood', 11);
---
+++ planet_insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO planet(star_id,has_life, name, planet_type, mass, age_in_millions_of_years)
+VALUES(2, FALSE, 'oak', 'gas giant', 5E5, 300.1),
+(2, FALSE, 'maple','dwarf', 5.4E5, 251.7),
+(2,FALSE, 'cedar','ice giant', 3.9E7, 400.2),
+(4,TRUE, 'birch', 'helium', 6.7E4, 20.1),
+(1, TRUE, 'aspen', 'ice giant', 6.3E7, 10.8),
+(1, TRUE, 'palm', 'dwarf', 3.8E8, 536.7),
+(3, TRUE, 'alder', 'Chthonian', 2.8E5, 33.5),
+(3,TRUE,'manzanita','lava', 4.4E6, 200.2),
+(5,TRUE, 'pine', 'rogue', 6.6E6, 44.1),
+(7,TRUE, 'bristlecone', 'protoplanet', 9.5E7, 501.4),
+(7,TRUE, 'redwood', 'terrestrial', 8.8E8, 449.0),
+(7,TRUE, 'fig', 'carbon',2.6E7, 24.6);
